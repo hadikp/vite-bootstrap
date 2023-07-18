6cc75a3a3e9eb4d50945548374a3c62314230894
hadikp <hp20001025@gmail.com>
2023-07-18 19:22:08 +0200

    add post store

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,3 +1,18 @@
+<script setup>
+import axios from 'redaxios'
+import { ref } from 'vue';
+import { usePostStore } from '../stores/post';
+
+const error = ref('');
+const postData = usePostStore()
+
+axios.get('api/post')
+.then(resp => {
+      postData.post = resp.data;
+     })
+    .catch(err => (error.value = 'Valami hiba történt, próbáld újra!'))
+</script>
+
 <template>
   <div class="about">
     <h1>This is a blog page</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,21 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  plugins: [vue()],
+  server: {
+    proxy: {
+    '/api': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+    },
   }
+}
+  
 })
