68a3f9aabf059864f96fcdf89d28cbaeb8ebf0a4
hadikp <hp20001025@gmail.com>
2023-07-19 18:43:28 +0200

    blog posts appear

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -14,17 +14,33 @@ axios.get('api/post')
 </script>
 
 <template>
-  <div class="about">
+  <div class="about d-flex flex-column">
     <h1>This is a blog page</h1>
+    <article v-for="post in postData.post" :key="post.id">
+      <div class="article-date">
+        <!-- <h3> Published on: {{ date(post.createdAt).format('YYYY-MM-DD HH:mm') }} </h3> -->
+      
+      <div class="article-head">
+        <h3>{{ post.title }}</h3>
+      </div>
+      <div class="article-body">
+        <!-- <img width="50" height="30" src="" alt="" /> -->
+        <p> {{ post.content }} </p>
+      </div>
+    </div>
+    </article>
+    <button type="button" class="btn btn-secondary btn-sm ">Small button</button>
+
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
+    margin: 1rem;
+    /* min-height: 100vh;
     display: flex;
-    align-items: center;
+    align-items: center; */
   }
 }
 </style>
