c0ad744fc122de8e1d8ab0100b9f71fd02f21e79
hadikp <hp20001025@gmail.com>
2023-07-21 20:28:25 +0200

    computed slice

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,13 +1,10 @@
 <script setup>
 import axios from 'redaxios'
-import { ref } from 'vue';
-import { usePostStore } from '../stores/post';
+import { ref, computed } from 'vue';
+import { usePostStore } from '../stores/post'
 
-const error = ref('');
+const error = ref('')
 const postData = usePostStore()
-
-const perPage = 5
-
 
 axios.get('api/post')
 .then(resp => {
@@ -15,14 +12,22 @@ axios.get('api/post')
      })
     .catch(err => (error.value = 'Valami hiba történt, próbáld újra!'))
 const totalRows = postData.post.length
-console.log() //totalRows
+const perPage = 2
+const pages = (page) => console.log(page)
+
+const slicePost = computed(() => postData.post.slice(0,2))
+
+
+  
+
+
+
 </script>
 
 <template>
   <div class="about d-flex flex-column">
     <h1>This is a blog page</h1>
-    <h3>{{ totalRows }}</h3>
-    <article v-for="post in postData.post" :key="post.id">
+    <article v-for="post in slicePost" :key="post.id">
       <div class="article-date">
         <!-- <h3> Published on: {{ date(post.createdAt).format('YYYY-MM-DD HH:mm') }} </h3> -->
       
@@ -35,19 +40,13 @@ console.log() //totalRows
       </div>
     </div>
     </article>
-    <nav aria-label="...">
+    <nav aria-label="Page navigation example">
   <ul class="pagination">
-    <li class="page-item disabled">
-      <span class="page-link">Previous</span>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item" aria-current="page">
-      <span class="page-link">2</span>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li @click="pages(1)" class="page-item"><a class="page-link" href="#">1</a></li>
+    <li @click="pages(2)" class="page-item"><a class="page-link" href="#">2</a></li>
+    <li @click="pages(3)" class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
   </ul>
 </nav>
     <button type="button" class="btn btn-secondary btn-sm ">Small button</button>
